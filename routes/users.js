a009c43f33fd5219b877bfb69488827cfc2464ca
erhanozalp <erhanozalp01@gmail.com>
2025-01-23 23:28:32 +0300

    push

--- routes/users.js
+++ routes/users.js
@@ -521,6 +521,730 @@ router.get("/get-coach/:id", async (req, res) => {
   }
 });
 
+router.post("/create-admin", async (req, res) => {
+  try {
+    const role = await Role.findOne({ name: "Admin" });
+    const {name, email, password} = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({
+      name,
+      email,
+      password: hashedPassword,
+      role: role._id
+    });
+
+    await user.save();
+    res.status(201).json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Sunucu hatası" });
+  }
+});
+
+router.put("/update-admin/:id", async (req, res) => {
+    
+  try {
+    const {name, email} = req.body;
+    const user = await User.findByIdAndUpdate(
+      req.params.id,
+      { name, email},
+      { new: true }
+    );
+    if (!user) {
+      return res.status(404).json({ message: "Kullanıcı bulunamadı" });
+    }
+
+
+    await user.save();
+    res.status(200).json({ message: "Kullanıcı başarıyla güncellendi", user });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Sunucu hatası" });
+  }
+});
+
+router.post("/create-referee", async (req, res) => {
+  const {
+    name,
+    surname,
+    gender,
+    birthDate,
+    fatherName,
+    motherName,
+    cityId,
+    roleId,
+    sportStartDate,
+    athleteLicenseNo,
+    email,
+    password,
+    identityNumber,
+    refereeStatus
+  } = req.body;
+
+  // Tüm gerekli alanların kontrolü
+  if (!name || !surname || !gender || !birthDate || !fatherName || 
+      !motherName || !cityId || !roleId || !sportStartDate || 
+      !email || !password || !identityNumber || !refereeStatus) {
+    return res.status(400).json({ message: "Tüm alanlar zorunludur" });
+  }
+
+  try {
+    // Role, City varlığını kontrol et
+    const [role, city] = await Promise.all([
+      Role.findById(roleId),
+      City.findById(cityId),
+    ]);
+
+    if (!role || !city) {
+      return res.status(404).json({ 
+        message: "Rol, şehir veya kulüp bulunamadı" 
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({
+      name,
+      surname,
+      gender,
+      birthDate,
+      fatherName,
+      motherName,
+      city: cityId,
+      role: roleId,
+      sportStartDate,
+      athleteLicenseNo,
+      email,
+      password: hashedPassword
+    });
+
+    await user.save();
+    
+    // Şifre hariç kullanıcı bilgilerini döndür
+    const userResponse = await User.findById(user._id)
+      .select('-password')
+      .populate(['role', 'city', 'club']);
+      
+    res.status(201).json(userResponse);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+router.post("/update-referee/:id", async (req, res) => {
+  const {
+    name,
+    surname,
+    gender,
+    birthDate,
+    fatherName,
+    motherName,
+    cityId,
+    roleId,
+    sportStartDate,
+    athleteLicenseNo,
+    email,
+    identityNumber,
+    bloodType,
+    religion,
+    nationality,
+    serialNumber,
+    educationStatus,
+    language,
+    bankInfo,
+    passportInfo,
+    workPhone,
+    workAddress,
+    homePhone,
+    homeAddress,
+    mobilePhone,
+    website,
+    showInStatistics,
+    licenseNo,
+    startDate,
+    province,
+    district,
+    institutionPosition,
+    isAthlete,
+    isVisuallyImpairedAthlete,
+    isHearingImpairedAthlete,
+    coachVisaYear,
+    isCoach,
+    coachStatus,
+    isReferee,
+    refereeVisaYear,
+    isProvincialRepresentative,
+    isStaff,
+    isBoardMember,
+    refereeStatus,
+  } = req.body;
+
+  // Tüm gerekli alanların kontrolü
+  if (!name || !surname || !gender || !birthDate || !fatherName || 
+    !motherName || !cityId || !roleId || !sportStartDate || 
+    !email || !identityNumber || !refereeStatus) {
+  return res.status(400).json({ message: "Tüm alanlar zorunludur" });
+}
+
+
+
+try {
+
+  // Role, City ve Club varlığını kontrol et
+  const [role, city] = await Promise.all([
+    Role.findById(roleId),
+    City.findById(cityId),
+  ]);
+
+  if (!role || !city) {
+    return res.status(404).json({ 
+      message: "Rol, şehir bulunamadı" 
+    });
+  }
+
+
+  // Güncelleme işlemi
+  const referee = await User.findById(req.params.id);
+  if (!referee) {
+    return res.status(404).json({ message: "Hakem bulunamadı" });
+  }
+
+
+
+  // Verilen tüm alanları güncelle
+  Object.assign(referee, {
+    name,
+    surname,
+    gender,
+    birthDate,
+    fatherName,
+    motherName,
+    city: cityId,
+    role: roleId,
+    sportStartDate,
+    athleteLicenseNo,
+    email,
+    identityNumber,
+    bloodType,
+    religion,
+    nationality,
+    serialNumber,
+    educationStatus,
+    language,
+    bankInfo,
+    passportInfo,
+    workPhone,
+    workAddress,
+    homePhone,
+    homeAddress,
+    mobilePhone,
+    website,
+    showInStatistics,
+    licenseNo,
+    startDate,
+    province,
+    district,
+    institutionPosition,
+    isAthlete,
+    isVisuallyImpairedAthlete,
+    isHearingImpairedAthlete,
+    coachVisaYear,
+    isCoach,
+    coachStatus,
+    isReferee,
+    refereeVisaYear,
+    isProvincialRepresentative,
+    isStaff,
+    isBoardMember,
+    refereeStatus
+  });
+
+  // Kaydet ve yanıt dön
+  await referee.save();
+
+  res.status(200).json({ message: "Hakem başarıyla güncellendi", referee });
+} catch (error) {
+  console.error(error);
+  res.status(500).json({ message: "Sunucu hatası" });
+}
+});
+
+router.post("/delete-referee/:id", async (req, res) => {
+  try {
+    const referee = await User.findByIdAndDelete(req.params.id);
+    if (!referee) {
+      return res.status(404).json({ message: "Hakem bulunamadı" });
+    }
+
+    res.status(200).json({ message: "Hakem başarıyla silindi" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Sunucu hatası" });
+  }
+}
+);
+
+router.get("/get-referees", async (req, res) => {
+  try {
+    const role = await Role.findOne({ name: "Referee" });
+    const referees = await User.find({ role: role._id })
+      .select("-password")
+      .populate(["role", "city"]);
+
+    res.status(200).json(referees);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Sunucu hatası" });
+  }
+});
+
+router.get("/get-referee/:id", async (req, res) => {
+    try {
+      const referee = await User.findById(req.params.id)
+        .select("-password")
+        .populate(["role", "city", "club"]);
+  
+      if (!referee) {
+        return res.status(404).json({ message: "Hakem bulunamadı" });
+      }
+  
+      res.status(200).json(referee);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Sunucu hatası" });
+    }
+  }
+  );
+
+router.post("/create-representetive", async (req, res) => {
+  const {
+    name,
+    surname,
+    gender,
+    birthDate,
+    fatherName,
+    motherName,
+    cityId,
+    roleId,
+    sportStartDate,
+    email,
+    password,
+    identityNumber
+  } = req.body;
+
+  // Tüm gerekli alanların kontrolü
+  if (!name || !surname || !gender || !birthDate || !fatherName || 
+      !motherName || !cityId || !roleId || !sportStartDate || 
+      !email || !password || !identityNumber) {
+    return res.status(400).json({ message: "Tüm alanlar zorunludur" });
+  }
+
+  try {
+    // Role, City ve Club varlığını kontrol et
+    const [role, city] = await Promise.all([
+      Role.findById(roleId),
+      City.findById(cityId),
+    ]);
+
+    if (!role || !city) {
+      return res.status(404).json({ 
+        message: "Rol, şehir veya kulüp bulunamadı" 
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({
+      name,
+      surname,
+      gender,
+      birthDate,
+      fatherName,
+      motherName,
+      city: cityId,
+      role: roleId,
+      sportStartDate,
+      email,
+      password: hashedPassword
+    });
+
+    await user.save();
+    
+    // Şifre hariç kullanıcı bilgilerini döndür
+    const userResponse = await User.findById(user._id)
+      .select('-password')
+      .populate(['role', 'city', 'club']);
+      
+    res.status(201).json(userResponse);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+router.post("/update-representetive/:id", async (req, res) => {
+  const {
+    name,
+    surname,
+    gender,
+    birthDate,
+    fatherName,
+    motherName,
+    cityId,
+    roleId,
+    sportStartDate,
+    email,
+    identityNumber,
+    bloodType,
+    religion,
+    nationality,
+    serialNumber,
+    educationStatus,
+    language,
+    bankInfo,
+    passportInfo,
+    workPhone,
+    workAddress,
+    homePhone,
+    homeAddress,
+    mobilePhone,
+    website,
+    showInStatistics,
+    licenseNo,
+    startDate,
+    province,
+    district,
+    institutionPosition,
+    isAthlete,
+    isVisuallyImpairedAthlete,
+    isHearingImpairedAthlete,
+    coachVisaYear,
+    isCoach,
+    coachStatus,
+    isReferee,
+    refereeVisaYear,
+    isProvincialRepresentative,
+    isStaff,
+    isBoardMember,
+    boardDuty,
+    promotion,
+    promotionDate,
+  } = req.body;
+
+  // Tüm gerekli alanların kontrolü
+  if (!name || !surname || !gender || !birthDate || !fatherName || 
+      !motherName || !cityId || !roleId || !sportStartDate || 
+      !email || !identityNumber) {
+    return res.status(400).json({ message: "Tüm alanlar zorunludur" });
+  }
+
+  try {
+    // Role, City ve Club varlığını kontrol et
+    const [role, city] = await Promise.all([
+      Role.findById(roleId),
+      City.findById(cityId),
+    ]);
+
+    if (!role || !city) {
+      return res.status(404).json({ 
+        message: "Rol, şehir veya kulüp bulunamadı" 
+      });
+    }
+
+    // Güncelleme işlemi
+    const representetive = await User.findById(req.params.id);
+    if (!representetive) {
+      return res.status(404).json({ message: "Temsilci bulunamadı" });
+    }
+
+    Object.assign(representetive, {
+      name,
+      surname,
+      gender,
+      birthDate,
+      fatherName,
+      motherName,
+      cityId,
+      roleId,
+      sportStartDate,
+      email,
+      identityNumber,
+      bloodType,
+      religion,
+      nationality,
+      serialNumber,
+      educationStatus,
+      language,
+      bankInfo,
+      passportInfo,
+      workPhone,
+      workAddress,
+      homePhone,
+      homeAddress,
+      mobilePhone,
+      website,
+      showInStatistics,
+      licenseNo,
+      startDate,
+      province,
+      district,
+      institutionPosition,
+      isAthlete,
+      isVisuallyImpairedAthlete,
+      isHearingImpairedAthlete,
+      coachVisaYear,
+      isCoach,
+      coachStatus,
+      isReferee,
+      refereeVisaYear,
+      isProvincialRepresentative,
+      isStaff,
+      isBoardMember,
+      boardDuty,
+      promotion,
+      promotionDate,
+    });
+
+    // Kaydet ve yanıt dön
+    await representetive.save();
+
+    res.status(200).json({ message: "Temsilci başarıyla güncellendi", representetive });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Sunucu hatası" });
+  }
+});
+
+router.post("/delete-representetive/:id", async (req, res) => {
+  try {
+    const representetive = await User.findByIdAndDelete(req.params.id);
+    if (!representetive) {
+      return res.status(404).json({ message: "Temsilci bulunamadı" });
+    }
+
+    res.status(200).json({ message: "Temsilci başarıyla silindi" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Sunucu hatası" });
+  }
+}
+);
+
+router.get("/get-representetives", async (req, res) => {
+  try {
+    const role = await Role.findOne({ name: "Representetive" });
+    const representetives = await User.find({ role: role._id })
+      .select("-password")
+      .populate(["role", "city"]);
+
+    res.status(200).json(representetives);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Sunucu hatası" });
+  }
+}
+);
+
+router.get("/get-representetive/:id", async (req, res) => {
+  try {
+    const representetive = await User.findById(req.params.id)
+      .select("-password")
+      .populate(["role", "city", "club"]);
+
+    if (!representetive) {
+      return res.status(404).json({ message: "Temsilci bulunamadı" });
+    }
+
+    res.status(200).json(representetive);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Sunucu hatası" });
+  }
+}
+);
+
+
+router.post("/create-personel", async (req, res) => {
+  const {
+    name,
+    surname,
+    gender,
+    birthDate,
+    fatherName,
+    motherName,
+    cityId,
+    clubId,
+    roleId,
+    sportStartDate,
+    athleteLicenseNo,
+    email,
+    password,
+    identityNumber
+  } = req.body;
+
+  // Tüm gerekli alanların kontrolü
+  if (!name || !surname || !gender || !birthDate || !fatherName || 
+      !motherName || !cityId || !clubId || !roleId || !sportStartDate || 
+      !email || !password || !identityNumber) {
+    return res.status(400).json({ message: "Tüm alanlar zorunludur" });
+  }
+
+  try {
+    // Role, City ve Club varlığını kontrol et
+    const [role, city, club] = await Promise.all([
+      Role.findById(roleId),
+      City.findById(cityId),
+      Club.findById(clubId)
+    ]);
+
+    if (!role || !city || !club) {
+      return res.status(404).json({ 
+        message: "Rol, şehir veya kulüp bulunamadı" 
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({
+      name,
+      surname,
+      gender,
+      birthDate,
+      fatherName,
+      motherName,
+      city: cityId,
+      club: clubId,
+      role: roleId,
+      sportStartDate,
+      athleteLicenseNo,
+      email,
+      password: hashedPassword
+    });
+
+    await user.save();
+    
+    // Şifre hariç kullanıcı bilgilerini döndür
+    const userResponse = await User.findById(user._id)
+      .select('-password')
+      .populate(['role', 'city', 'club']);
+      
+    res.status(201).json(userResponse);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+router.post("/update-personel/:id", async (req, res) => {
+  const {
+    name,
+    surname,
+    gender,
+    birthDate,
+    fatherName,
+    motherName,
+    cityId,
+    clubId,
+    roleId,
+    sportStartDate,
+    athleteLicenseNo,
+    email,
+    password,
+    identityNumber
+  } = req.body;
+
+  // Tüm gerekli alanların kontrolü
+  if (!name || !surname || !gender || !birthDate || !fatherName || 
+      !motherName || !cityId || !clubId || !roleId || !sportStartDate || 
+      !email || !password || !identityNumber) {
+    return res.status(400).json({ message: "Tüm alanlar zorunludur" });
+  }
+
+  try {
+    // Role, City ve Club varlığını kontrol et
+    const [role, city, club] = await Promise.all([
+      Role.findById(roleId),
+      City.findById(cityId),
+      Club.findById(clubId)
+    ]);
+
+    if (!role || !city || !club) {
+      return res.status(404).json({ 
+        message: "Rol, şehir veya kulüp bulunamadı" 
+      });
+    }
+
+    // Güncelleme işlemi
+    const personel = await User.findById(req.params.id);
+    if (!personel) {
+      return res.status(404).json({ message: "Personel bulunamadı" });
+    }
+
+    personel.name = name;
+    personel.surname = surname;
+    personel.gender = gender;
+    personel.birthDate = birthDate;
+    personel.fatherName = fatherName;
+    personel.motherName = motherName;
+    personel.city = cityId;
+    personel.club = clubId;
+    personel.role = roleId;
+    personel.sportStartDate = sportStartDate;
+    personel.athleteLicenseNo = athleteLicenseNo;
+    personel.email = email;
+    personel.identityNumber = identityNumber;
+
+    await personel.save();
+
+    res.status(200).json({ message: "Personel başarıyla güncellendi", personel });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Sunucu hatası" });
+  }
+});
+
+router.post("/delete-personel/:id", async (req, res) => {
+  try {
+    const personel = await User.findByIdAndDelete(req.params.id);
+    if (!personel) {
+      return res.status(404).json({ message: "Personel bulunamadı" });
+    }
+
+    res.status(200).json({ message: "Personel başarıyla silindi" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Sunucu hatası" });
+  }
+}
+);
+
+router.get("/get-personels", async (req, res) => {
+  try {
+    const personels = await User.find({ role: "Personel" })
+      .select("-password")
+      .populate(["role", "city", "club"]);
+
+    res.status(200).json(personels);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Sunucu hatası" });
+  }
+}
+);
+
+router.get("/get-personel/:id", async (req, res) => {
+  try {
+    const personel = await User.findById(req.params.id)
+      .select("-password")
+      .populate(["role", "city", "club"]);
+
+    if (!personel) {
+      return res.status(404).json({ message: "Personel bulunamadı" });
+    }
+
+    res.status(200).json(personel);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Sunucu hatası" });
+  }
+}
+);
+
 
 
 // Login a user

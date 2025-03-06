efb12253751e42c951a373cbef433c1b77a7be0d
c19ye <80114964+c19ye@users.noreply.github.com>
2025-03-07 01:08:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 
 // CORS middleware'i tüm route'lardan önce gelmeli
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', process.env.ALLOWED_ORIGIN || '*');
+  res.setHeader('Access-Control-Allow-Origin', process.env.ALLOWED_ORIGIN || 'http://192.168.137.1:3000');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', [
     'Content-Type',
